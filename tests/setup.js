12ebba651c659030f8bf2d4cc93259affe339dd8
Dessy <dskdedzoe@st.ug.edu.gh>
2026-03-05 08:42:10 +0000

    initial commit

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,16 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import app from "../src/app.js";
+
+dotenv.config();
+
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+}, 30000);
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+});
 
 export default app;
